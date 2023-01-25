974509352757340d3da9b54079cf57bf794fa13e
sergii13 <sergii.khilchuk13@gmail.com>
2023-01-25 21:13:06 +0200

    fux bug and loader only main page

--- src/js/files/functions.js
+++ src/js/files/functions.js
@@ -26,11 +26,11 @@ export function addTouchClass() {
 }
 // Додавання loaded для HTML після повного завантаження сторінки
 export function addLoadedClass() {
-   if (!document.documentElement.classList.contains('loading')) {
+   if (document.documentElement.classList.contains('loading')) {
       window.addEventListener("load", function () {
          setTimeout(function () {
             document.documentElement.classList.add('loaded');
-         }, 2000);
+         }, 700);
       });
    }
 }
