7d362a1f1ddcdbec1a6e100d2a1355654e341bcf
sergii13 <sergii.khilchuk13@gmail.com>
2023-01-26 16:50:58 +0200

    fix swiper

---
+++ dist/main.c338fec3d3ee384a2a82.hot-update.js
@@ -0,0 +1,22 @@
+self["webpackHotUpdatefls_start"]("main",{
+
+/***/ "./node_modules/string-replace-loader/index.js??ruleSet[1].rules[0].use[1]!./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[0].use[2]!./node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[0].use[3]!./src/scss/style.scss":
+/*!********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/string-replace-loader/index.js??ruleSet[1].rules[0].use[1]!./node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[0].use[2]!./node_modules/sass-loader/dist/cjs.js??ruleSet[1].rules[0].use[3]!./src/scss/style.scss ***!
+  \********************************************************************************************************************************************************************************************************************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Expected identifier.\n   ╷\n50 │    &__slide..swiper-slide{\n   │             ^\n   ╵\n  src/scss/sections/gallery.scss 50:13  @import\n  src/scss/sections.scss 1:9            @import\n  src/scss/style.scss 185:9             root stylesheet");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("e53e6aa36111c97ab664")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5jMzM4ZmVjM2QzZWUzODRhMmE4Mi5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2Zscy1zdGFydC93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiZTUzZTZhYTM2MTExYzk3YWI2NjRcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
